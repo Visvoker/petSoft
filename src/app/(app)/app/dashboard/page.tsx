3fba23e773dac9b0d620f2728a48df93fc13fc3a
visvoker <s2089533@gmail.com>
2025-05-21 13:53:18 +0800

    Branding + Stats Components

--- src/app/(app)/app/dashboard/page.tsx
+++ src/app/(app)/app/dashboard/page.tsx
@@ -1,5 +1,14 @@
+import Branding from "@/components/branding";
+import Stats from "@/components/stats";
+
 export default function DashboardPage() {
   return (
-    <div>DashboardPage</div>
-  )
+    <main>
+      <div className="flex items-center justify-between text-white py-8 ">
+        <Branding />
+
+        <Stats />
+      </div>
+    </main>
+  );
 }
