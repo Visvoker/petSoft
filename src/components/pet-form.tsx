ad671794580faa38ae9c0b6bf8e32f2fe7084e2c
visvoker <s2089533@gmail.com>
2025-05-30 12:11:09 +0800

    Validation with Native React-Hook-Form

--- src/components/pet-form.tsx
+++ src/components/pet-form.tsx
@@ -30,11 +30,15 @@ export default function PetForm({
   const {
     register,
     formState: { isSubmitting, errors },
+    trigger,
   } = useForm<TPetForm>();
 
   return (
     <form
       action={async (formData) => {
+        const result = await trigger();
+        if (!result) return;
+
         onFormSubmission();
 
         const petData = {
@@ -57,13 +61,31 @@ export default function PetForm({
       <div className="space-y-3">
         <div className="space-y-1">
           <Label htmlFor="name">name</Label>
-          <Input id="name" {...register("name")} />
+          <Input
+            id="name"
+            {...register("name", {
+              required: "Name is require",
+              minLength: {
+                value: 3,
+                message: "Name must be at least 3 characters long",
+              },
+            })}
+          />
           {errors.name && <p className="text-red-500">{errors.name.message}</p>}
         </div>
 
         <div className="space-y-1">
           <Label htmlFor="ownerName">Owner Name</Label>
-          <Input id="ownerName" {...register("ownerName")} />
+          <Input
+            id="ownerName"
+            {...register("ownerName", {
+              required: "ownerName is require",
+              maxLength: {
+                value: 20,
+                message: "OwnerName must be less than 20 characters long",
+              },
+            })}
+          />
           {errors.ownerName && (
             <p className="text-red-500">{errors.ownerName.message}</p>
           )}
