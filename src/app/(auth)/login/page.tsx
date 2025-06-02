7ea90f8b3509e53b2b1aeddc02288c559d5dd056
visvoker <s2089533@gmail.com>
2025-06-02 19:39:40 +0800

    Finish Auth Pages

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,5 +1,21 @@
+import AuthForm from "@/components/auth-form";
+import H1 from "@/components/h1";
+import Link from "next/link";
+
 export default function LoginPage() {
   return (
-    <div>LoginPage</div>
-  )
+    <main>
+      <H1 className="mb-5 text-center">Log In</H1>
+
+      <AuthForm />
+
+      <p className="mt-6 text-sm text-zinc-500">
+        No account yet?{" "}
+        <Link href={"/signup"} className="font-medium">
+          Sign up
+        </Link>
+      </p>
+
+    </main>
+  );
 }
--- src/components/h1.tsx
+++ src/components/h1.tsx
@@ -1,5 +1,4 @@
 import { cn } from "@/lib/utils";
-import React, { ReactNode } from "react";
 
 type H1Props = {
   children: React.ReactNode;
@@ -8,7 +7,7 @@ type H1Props = {
 
 export default function H1({ children, className }: H1Props) {
   return (
-    <h1 className={(cn("font-medium text-2xl leading-6"), className)}>
+    <h1 className={cn("font-medium text-2xl leading-6", className)}>
       {children}
     </h1>
   );
