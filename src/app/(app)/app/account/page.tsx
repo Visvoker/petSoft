c4d481f420d9bf510f8e718c0e156e2009fd6273
visvoker <s2089533@gmail.com>
2025-06-05 14:20:33 +0800

    Render user email in UI

--- src/app/(app)/app/account/page.tsx
+++ src/app/(app)/app/account/page.tsx
@@ -1,13 +1,21 @@
 import ContentBlock from "@/components/content-block";
 import H1 from "@/components/h1";
+import auth from "@/middleware";
+import { redirect } from "next/navigation";
 
-export default function AccountPage() {
+export default async function AccountPage() {
+  const section = await auth();
+
+  if (!section?.user) {
+    redirect("/login");
+  }
+
   return (
     <main>
       <H1 className="my-8 text-white">Your account</H1>
 
       <ContentBlock className="h-[500px] flex justify-center items-center">
-        <p> Logged in as ...</p>
+        <p> Logged in as ...{section.user.email}</p>
       </ContentBlock>
     </main>
   );
