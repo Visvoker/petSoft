6ccd2bc8a418dea42e4541da622252138a0c98c6
visvoker <s2089533@gmail.com>
2025-06-05 14:53:26 +0800

    Sign out(button+server action)

--- src/app/(app)/app/account/page.tsx
+++ src/app/(app)/app/account/page.tsx
@@ -1,5 +1,7 @@
 import ContentBlock from "@/components/content-block";
 import H1 from "@/components/h1";
+import SignOutBtn from "@/components/sign-out-btn";
+
 import auth from "@/middleware";
 import { redirect } from "next/navigation";
 
@@ -14,8 +16,9 @@ export default async function AccountPage() {
     <main>
       <H1 className="my-8 text-white">Your account</H1>
 
-      <ContentBlock className="h-[500px] flex justify-center items-center">
+      <ContentBlock className="h-[500px] flex flex-col gap-3 justify-center items-center">
         <p> Logged in as ...{section.user.email}</p>
+        <SignOutBtn />
       </ContentBlock>
     </main>
   );
