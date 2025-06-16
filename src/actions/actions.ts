57512965e1c456557b210e5212a4a8881d67e40c
visvoker <s2089533@gmail.com>
2025-06-16 21:08:23 +0800

    NextJS Redirect Quirk

--- src/actions/actions.ts
+++ src/actions/actions.ts
@@ -33,17 +33,13 @@ export async function logIn(prevState: unknown, formData: unknown) {
         }
         default: {
           return {
-            message: "Could not sign in.",
+            message: "Error.Could not sign in.",
           };
         }
       }
     }
-    return {
-      message: "Could not sign in.",
-    };
+    throw error; //nextjs redirects throws error,so we need to rethrow it
   }
-
-  redirect("app/dashboard");
 }
 
 export async function logOut() {
