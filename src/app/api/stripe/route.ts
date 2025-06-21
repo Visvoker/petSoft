b67445039c8bc01bdcc1bd2926d7805becb82eae
visvoker <s2089533@gmail.com>
2025-06-21 12:13:28 +0800

    Receive webhooks locally(Ngrok)/fix webhook mistake

--- src/app/api/stripe/route.ts
+++ src/app/api/stripe/route.ts
@@ -1,4 +1,9 @@
 export async function POST(request: Request) {
   const body = await request.text();
   console.log(body);
+
+  // fulfill order
+
+  // return 200 ok
+  return Response.json(null, { status: 200 });
 }
