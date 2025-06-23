f52af153a9944995cb032c2cbec83a6f5693f0a2
visvoker <s2089533@gmail.com>
2025-06-23 14:58:18 +0800

    Fulfill order(database update)

--- src/app/api/stripe/route.ts
+++ src/app/api/stripe/route.ts
@@ -1,8 +1,19 @@
+import prisma from "@/lib/db";
+
 export async function POST(request: Request) {
-  const body = await request.text();
-  console.log(body);
+  const data = await request.json();
+
+  // verify webhook from Stripe
 
   // fulfill order
+  await prisma.user.update({
+    where: {
+      email: data.data.object.customer_email,
+    },
+    data: {
+      hasAccess: true,
+    },
+  });
 
   // return 200 ok
   return Response.json(null, { status: 200 });
